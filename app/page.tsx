4eaa3f1fdde4f4b350e31419344cbea601ed4416
ayeni olakunle <ayeniolakunle98@gmail.com>
2024-06-06 18:20:10 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,97 @@
-import Image from "next/image";
+"use client";
+
+import axios from "axios";
+import { useState } from "react";
 
 export default function Home() {
+  const [link, setLink] = useState<string>("");
+  const [link2, setLink2] = useState<string>("");
+  const [load, setLoad] = useState<boolean>(false);
+  const [copy, setCopy] = useState<boolean>(false);
+
+  const postRequest = (e: any) => {
+    e.preventDefault();
+    setLoad(true);
+
+    const options = {
+      url: "https://shortlink-virid.vercel.app/",
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data: {
+        originalUrl: link,
+      },
+    };
+
+    axios(options)
+      .then((response) => {
+        setLoad(false);
+        setLink("");
+        console.log(response.data.shortUrl);
+        setLink2(
+          `https://shortlink-virid.vercel.app/${response.data.shortUrl}`
+        );
+      })
+      .catch((err) => {
+        setLoad(false);
+        setCopy(false);
+        alert("Opps something went wrong or link already exist");
+      });
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="w-full h-screen flex justify-center items-center">
+      <div className="w-1/2 flex justify-center items-center flex-col gap-[30px]">
+        <h1 className="text-[25px] font-bold">
+          Shorten your links with link shortener
+        </h1>
+        <form
+          className="w-full flex justify-center items-center gap-[30px]"
+          onSubmit={postRequest}
+        >
+          <input
+            type="url"
+            className="border-[1px] border-solid border-[#e0e0e0] [box-shadow:5px_5px_5px_#e0e0e0,_-5px_-5px_5px_#e0e0e0] w-4/5 p-[15px] rounded-[50px] outline-[0]"
+            value={link}
+            onChange={(e) => setLink(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="w-1/5 p-[15px] rounded-[50px] bg-[#3F51B5] text-[white] font-medium"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            {load ? "Please wait..." : "Submit"}
+          </button>
+        </form>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {link2 !== "" && (
+          <div className="w-full flex justify-center items-center flex-col gap-[10px]">
+            <h1 className="text-[20px] font-bold text-[#009688]">
+              Copy your new shortened link
+            </h1>
+            <div className="border-[1px] border-solid border-[#e0e0e0] [box-shadow:5px_5px_5px_#e0e0e0,_-5px_-5px_5px_#e0e0e0] px-[17px] py-[10px] w-full flex justify-between items-center rounded-[50px]">
+              <p>{link2}</p>
+              <div className="flex justify-center items-center gap-[10px]">
+                <a
+                  href={link2}
+                  target="_blank"
+                  className="p-[15px] rounded-[50px] bg-[#009688] text-[white] font-medium px-[30px] py-[15px]"
+                >
+                  Open
+                </a>
+                <button
+                  className="p-[15px] rounded-[50px] bg-[#3F51B5] text-[white] font-medium px-[30px] py-[15px]"
+                  onClick={() => {
+                    setCopy(true);
+                    navigator.clipboard.writeText(link2);
+                  }}
+                >
+                  {copy ? "Copied" : "Copy"}
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </main>
   );
